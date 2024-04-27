8268d30f84c65275d2d7e039b65e123382bf1925
Falko Döhring <github@falko.in>
2024-04-27 13:54:38 +0200

    feat: add reverse function

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import typer
 
 app = typer.Typer()
 
+def say_reverse(name: str) -> str:
+    name_reverse = name[::-1]
+    return f"Hi, {name_reverse}! You are awesome!"
 
 def say_hi(name: str) -> str:
     return f"Hi, {name}! You are awesome!"
@@ -12,6 +15,9 @@ def say_hi(name: str) -> str:
 def greet(name: Annotated[str, typer.Argument()]):
     print(say_hi(name))
 
+@app.command()
+def teerg(name: Annotated[str, typer.Argument()]):
+    print(say_reverse(name))
 
 if __name__ == "__main__":
     app()
